4a1394b6cd666b455f121d9fd0f8ca0c4d45f6e5
MahammadFayazShaik <skfayaz1999@gmail.com>
2024-09-12 11:29:14 +0530

    Final update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   //UseStates Variables
   const [audioProgress, setAudioProgress] = useState(0);
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
-  const [musicIndex, setMusicIndex] = useState(0);
+
   const [musicTotalLength, setMusicTotalLength] = useState('04 : 38');
   const [musicCurrentTime, setMusicCurrentTime] = useState('00 : 00');
   const [videoIndex, setVideoIndex] = useState(0)
@@ -30,17 +30,6 @@ function App() {
   const handleMusicProgressBar = (e)=>{
     setAudioProgress(e.target.value);
     currentAudio.current.currentTime = e.target.value * currentAudio.current.duration / 100;
-  }
-
-  //Change Avatar Class
-  let avatarClass = ['objectFitCover','objectFitContain','none']
-  const [avatarClassIndex, setAvatarClassIndex] = useState(0)
-  const handleAvatar = ()=>{
-    if (avatarClassIndex >= avatarClass.length - 1) {
-      setAvatarClassIndex(0)
-    }else{
-      setAvatarClassIndex(avatarClassIndex + 1)
-    }
   }
 
 
